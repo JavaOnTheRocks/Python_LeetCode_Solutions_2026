bb3a4338f58d5fdbe53c7ef8cfc38da108a58956
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-07-02 07:48:48 +0530

    Time: 115 ms (93.62%), Space: 66.5 MB (59%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,13 +5,38 @@
 #         self.next = next
 class Solution(object):
     def isPalindrome(self, head):
-        #appraoch 1
-        curr=head
-        arr=[]
-        while curr != None:
-            arr.append(curr.val)
-            curr=curr.next
-        reverse_arr= arr[::-1]# with th help of slicing
-        return arr == reverse_arr
-        #time complexity = o(n)=o(n)+o(n)+o(n)
-        # space complexity = o(n)
+        slow=head
+        fast=head
+        while fast!= None and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        #Use three pointer to reverse the LinkedList:
+        curr=slow
+        prev=None
+        nxt=None
+        while curr:
+            nxt=curr.next
+            curr.next=prev
+            prev=curr
+            curr=nxt
+        left=head
+        right=prev
+
+        while right:
+            if left.val != right.val:
+                return False
+            left=left.next
+            right=right.next
+        return True
+        
+
+        # #appraoch 1
+        # curr=head
+        # arr=[]
+        # while curr != None:
+        #     arr.append(curr.val)
+        #     curr=curr.next
+        # reverse_arr= arr[::-1]# with th help of slicing
+        # return arr == reverse_arr
+        # #time complexity = o(n)=o(n)+o(n)+o(n)
+        # # space complexity = o(n)
