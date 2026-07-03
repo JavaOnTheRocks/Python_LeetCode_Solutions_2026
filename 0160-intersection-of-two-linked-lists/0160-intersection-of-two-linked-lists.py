890bc0b2e9c9d625c70abeb6258a08efa56d8f18
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-07-03 06:42:08 +0530

    Time: 226 ms (98.49%), Space: 66.9 MB (46.29%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -5,38 +5,50 @@
 #         self.next = None
 
 class Solution(object):
-    def getlen(self,head):
-        l=0
-        curr=head
-        while curr.next!=None:
-            curr=curr.next
-            l+=1
-        l+=1
-        return l
+    # def getlen(self,head):
+    #     l=0
+    #     curr=head
+    #     while curr.next!=None:
+    #         curr=curr.next
+    #         l+=1
+    #     l+=1
+    #     return l
     def getIntersectionNode(self, headA, headB):
-        lenA=self.getlen(headA)
-        lenB=self.getlen(headB)
-        
         p1=headA
         p2=headB
-        
-        #align them
-        if lenA>lenB:
-            diff=lenA-lenB
-            for i in range(diff):
+        while p1!=p2:
+            if p1:
                 p1=p1.next
-        else:
-            diff=lenB-lenA
-            for i in range(diff):
+            else:
+                p1=headB
+            if p2:
                 p2=p2.next
+            else:
+                p2=headA
+        return p1
+        # lenA=self.getlen(headA)
+        # lenB=self.getlen(headB)
+        
+        # p1=headA
+        # p2=headB
+        
+        # #align them
+        # if lenA>lenB:
+        #     diff=lenA-lenB
+        #     for i in range(diff):
+        #         p1=p1.next
+        # else:
+        #     diff=lenB-lenA
+        #     for i in range(diff):
+        #         p2=p2.next
 
-        #move togather
-        while p1 and p2:
-            if p1==p2:
-                return p1
-            p1=p1.next
-            p2=p2.next
-        return None # No intersection 
+        # #move togather
+        # while p1 and p2:
+        #     if p1==p2:
+        #         return p1
+        #     p1=p1.next
+        #     p2=p2.next
+        # return None # No intersection 
         
 
         
