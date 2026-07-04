ba2555e13698dd3dc3506a0f95ab55910b8852c6
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-07-04 08:43:22 +0530

    Time: 19 ms (5.25%), Space: 12.5 MB (0.78%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def nextnumber(self,n):
+        total=0
+        while n>0:
+            digit=n%10 # provide last digit
+            total+=digit**2
+            n=n//10 #her baar last digit remove ker dega
+        return total
+    def isHappy(self, n):
+        slow=n
+        fast=n
+        # if n==1:
+        #     return True
+        while True:
+            slow=self.nextnumber(slow)
+            fast=self.nextnumber(self.nextnumber(fast))
+            if slow==1 or fast==1:
+                return True
+                break
+            if slow==fast:
+                return False
+                break
+
+          
+        
+
+        
+
