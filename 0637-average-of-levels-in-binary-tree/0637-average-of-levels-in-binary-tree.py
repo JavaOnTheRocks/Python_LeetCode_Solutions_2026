f4742405ac47d0073a875926155dde422bab2a25
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-07-27 15:31:01 +0530

    Time: 11 ms (9.52%), Space: 17.1 MB (20.11%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -0,0 +1,84 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self.count=0 #size count karna ka liya 
+
+    #Insert Element at rear:
+    def enqueue(self,data):
+        newNode=Node(data)
+        if self.front is None:
+            self.front=newNode
+            self.rear=newNode
+            self.count+=1
+            return
+        self.rear.next=newNode
+        self.rear=newNode
+        self.count+=1
+    
+    #Remove From Front:
+    def dequeue(self):
+        if self.front is None:
+            return None
+        removed=self.front.data
+        self.front=self.front.next
+        self.count-=1
+        return removed
+    
+    #Return From Front:
+    def peek(self):
+        if self.front is None:
+            return None
+        return self.front.data
+
+    def size(self):
+        if self.front is None:
+            return 0
+
+        return self.count
+    
+    #Check is Empty:
+    def isEmpty(self):
+        return self.front is None
+
+class Solution(object):
+    def averageOfLevels(self, root):
+        queue=Queue()
+
+        if root is None:
+            return []
+
+        ans=[]
+        ans.append(root.val)
+        queue.enqueue(root)
+
+        while queue.size()>0:
+            l=queue.size()
+            level=[]
+            for i in range(l):
+                front=queue.dequeue()
+                if front.left != None:
+                    level.append(front.left.val)
+                    queue.enqueue(front.left)
+                if front.right != None:
+                    level.append(front.right.val)
+                    queue.enqueue(front.right)
+            if len(level)>0:
+                total=float(sum(level))/len(level)
+                ans.append(total)
+        return ans
+
+
+        
+        
