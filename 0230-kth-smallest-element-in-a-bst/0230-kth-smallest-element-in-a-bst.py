1217e36fb373e4b180e9c7c23bf52838f37fb8d7
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-08-03 16:56:47 +0530

    Time: 6 ms (29.09%), Space: 20.2 MB (35.91%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,18 +6,41 @@
 #         self.right = right
 class Solution(object):
     def __init__(self):
-        self.ans=[]
-    def inorder(self,root):
+        self.count=0
+        self.result=0
+    def inorder(self,root,k):
         #Base case
         if root is None:
             return
-        self.inorder(root.left) 
-        self.ans.append(root.val)
-        self.inorder(root.right)
+
+        self.inorder(root.left,k) 
+
+        self.count+=1
+        if self.count == k:
+            self.result=root.val
+            return
+
+        self.inorder(root.right,k)
+
     def kthSmallest(self, root, k):
-        self.inorder(root)
-        return self.ans[k-1] # k-1 th index
-    ## Time and Space complexity of this code is o(n) ,o(n)
+        self.inorder(root,k)
+        return self.result
+    ## Appraoch 1
+
+    # def __init__(self):
+    #     self.ans=[]
+    # def inorder(self,root):
+    #     #Base case
+    #     if root is None:
+    #         return
+    #     self.inorder(root.left) 
+    #     self.ans.append(root.val)
+    #     self.inorder(root.right)
+    # def kthSmallest(self, root, k):
+    #     self.inorder(root)
+    #     return self.ans[k-1] # k-1 th index
+    # ## Time and Space complexity of this code is o(n) ,o(n)
+    
 
         
 
