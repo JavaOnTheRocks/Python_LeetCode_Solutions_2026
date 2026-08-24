13cc52b63de60d0b5afe4ccee8af4368cdb5fa79
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-08-24 13:54:55 +0530

    Time: 1123 ms (34.66%), Space: 75.8 MB (54.03%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,25 @@
+import heapq
+class Solution(object):#we will solve without building the graph
+    def minCostConnectPoints(self, points):
+        n=len(points)
+        min_cost=0
+        inMST=set()
+        #inMST=[False]*n # for this we also have to caeck how much we have visted till now in MST
+        heap=[]
+        heapq.heappush(heap,(0,0))
+
+        while len(inMST)<n:
+            wt,u=heapq.heappop(heap)
+
+            if u in inMST:
+                continue
+
+            inMST.add(u)
+            min_cost+=wt
+            #dynamically check for the neighbour 
+            for v in range(n):
+                if v not in inMST:
+                    dist = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1])
+                    heapq.heappush(heap,(dist,v))       
+        return min_cost
+        
