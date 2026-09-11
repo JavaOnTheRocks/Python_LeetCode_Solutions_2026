ae41c2702eec1aaf2d7b73a0f2ddb3d55efccbc3
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-09-11 10:49:22 +0530

    Time: 23 ms (42.99%), Space: 12.5 MB (17.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,8 +1,22 @@
 class Solution(object):
-    def fib(self, n):
+    #Recusion in this function
+    #We are not making the dp a attribute of this class so we are passing the dp in solve function,we can make a attribute using self.
+    def solve(self,n,dp):
+        #Base case
         if n == 0 or n==1:
             return n
+        
+        if dp[n]!=-1:
+            return dp[n]
+        
 
-        return self.fib(n-1) + self.fib(n-2)
+        dp[n]=self.solve(n-1,dp) + self.solve(n-2,dp)
+        return dp[n]
+
+    #def solve(n)
+    def fib(self, n):
+        dp=[-1]*(n+1)
+        
+        return self.solve(n,dp)
 
 #__import__("atexit").register(lambda:open("display_runtime.txt","w").write("0"))
