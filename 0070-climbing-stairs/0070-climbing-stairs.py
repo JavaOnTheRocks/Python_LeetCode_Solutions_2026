52a7eea842051a0c93fc938824b36c765a49b3b7
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-09-12 10:17:50 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.93%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,19 @@
 class Solution(object):
-    def solve(self,n,dp):
+    def climbStairs(self, n):
         if n==1 or n==2:
             return n
+            
+        dp=[-1]*(n+1)
+        
+        #tabulization base case 
+        dp[1]=1
+        dp[2]=2
+        # counting the all dp array values 
+        for i in range(3,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
 
-        if dp[n]!=-1:
-            return dp[n]
-
-        dp[n]=self.solve(n-1,dp) + self.solve(n-2,dp)
         return dp[n]
 
-    def climbStairs(self, n):
-        dp=[-1]*(n+1)#1D DP 
-
-        return self.solve(n,dp)
+        # dp=[-1]*(n+1)#1D DP 
+        # return self.solve(n,dp)
         
