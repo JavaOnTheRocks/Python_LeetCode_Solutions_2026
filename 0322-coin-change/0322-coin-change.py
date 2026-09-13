225594c70a853b6c108ee8bbef8884f40493d76e
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-09-13 11:04:32 +0530

    Time: 1648 ms (8.77%), Space: 22.3 MB (13.46%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def solveRec(self,coins,amount,memo):
+    def solveRec(self,coins,amount,dp):
         #base case
         if amount==0:
             return 0
@@ -7,23 +7,22 @@ class Solution(object):
         if amount < 0:
             return float("inf")
 
-        if amount in memo:#if amount already exist in memo
-            return memo[amount]
+        if dp[amount] != -1:#if amount already exist in memo
+            return dp[amount]
 
         minCount=float("inf")
         for coin in coins:
-            res=self.solveRec(coins,amount-coin,memo)
+            res=self.solveRec(coins,amount-coin,dp)
 
             if res != float("inf"):   #we are just saving from chaking becouse min hi min rah jayega is case ma
                 minCount=min(minCount,res+1) 
-        memo[amount]=minCount
+        dp[amount]=minCount
         return minCount
     def coinChange(self, coins, amount):
-        memo={}
-        ans=self.solveRec(coins,amount,memo)
+        dp=[-1]*(amount+1)
+        ans=self.solveRec(coins,amount,dp)
         if ans == float("inf"):
             return -1
         return ans
-
 
         
