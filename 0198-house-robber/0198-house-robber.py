d3dd6bb213d75841fa10b21e81b33ebc5544d485
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-09-14 12:36:23 +0530

    Time: 3 ms (7.02%), Space: 12.4 MB (21.9%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,21 +1,16 @@
 class Solution(object):
-    def solve(self,nums,i,dp):
-        #base case
-        if i < 0:#index out of bound 
-            return 0
-        if i == 0:
-            return nums[0]
-        if dp[i] != -1:
-            return dp[i]
-        #recursive case
-        include=self.solve(nums,i-2,dp)+nums[i]
-        exclude=self.solve(nums,i-1,dp)+0
-        dp[i]=max(include,exclude)
-        return dp[i]
-
     def rob(self, nums):
         n=len(nums)
-        dp=[-1]*(n+1)
-        return self.solve(nums,n-1,dp)#last index pa khada ha initally
+        if n == 0:#index out of bound 
+            return 0
+        if n == 1:
+            return nums[0]
+        dp=[0]*(n)
+        dp[0]=nums[0]#from base case
+        for i in range(1,n):
+            include=dp[i-2]+nums[i]
+            exclude=dp[i-1]+0
+            dp[i]=max(include,exclude)
+        return dp[i]
         
         
