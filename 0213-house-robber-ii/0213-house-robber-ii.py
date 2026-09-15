ddb7287bd7497e1b9324ebf9288a7472b3e27e71
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-09-15 15:51:04 +0530

    Time: 0 ms (100%), Space: 12.4 MB (61.14%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -11,18 +11,33 @@ class Solution(object):
         dp[i]=max(include,exclude)
         return dp[i]
 
+    def tabulization(self,nums):
+        n=len(nums)
+        if n==0:
+            return 0
+        if n==1:
+            return nums[0]
+        dp=[0]*n
+        #base case to bottoum 
+        dp[0]=nums[0]
+        for i in range(1,n):
+            include=dp[i-2]+nums[i]
+            exclude=dp[i-1]+0
+            dp[i]=max(include,exclude)
+
+        return dp[n-1]      
+
     def rob(self, nums):
         n=len(nums)
         if n==1:
             return nums[0]
-        #exclude last house 
-        list1=nums[0:n-1]
-        dp=[-1]*len(list1)
-        case1=self.Solve(list1,n-2,dp)
-        #exclude first house
-        list2=nums[1:n]
-        dp=[-1]*len(list2)
-        case2=self.Solve(list2,n-2,dp)
+        #case 1 : exclude last house 
+        list1=nums[:n-1]
+        case1=self.tabulization(list1)
+        #case 2 : exclude first house
+        list2=nums[1:]
+        case2=self.tabulization(list2)
         return max(case1,case2)
+
 
         
