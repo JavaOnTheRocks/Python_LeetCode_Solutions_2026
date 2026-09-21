9b5b8ea90e9d84a02bf237c7a4154473f613c818
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-09-21 16:13:08 +0530

    Time: 10 ms (75.43%), Space: 14 MB (98.19%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -10,6 +10,16 @@ class Solution(object):
 ##USING Python in build Sort() - O(NlogN)
         nums.sort()
         return nums
+## USING Bubble Sort   Wrost case T.C. - O(n^2),Best Case T.C. - O(n)
+        # has_swapped=True
+        # while has_swapped:
+        #     has_swapped=False
+        #     for i in range(n-1):
+        #         if nums[i]>nums[i+1]:
+        #             nums[i],nums[i+1]=nums[i+1],nums[i]
+        #             has_swapped=True
+        # return nums
+
 
 ## Using Selection sort:-
         # for i in range(n):
@@ -25,6 +35,9 @@ class Solution(object):
 ## Require Optimization....
 # Option 1 :
 # use python in Build sort - time complexity will be O(NlogN) Timsort
+## option 2 : different sorting ALgorithum
+## option 3 : Two Pointer Appraoch
+
 # Space complexity - O(1)
         
         
