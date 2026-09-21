4c9cd0e8313872481028618d9c28469e369f1b1c
RAKESH BISHNOI <rbisnnoii@gmail.com>
2026-09-21 16:18:51 +0530

    Time: 11 ms (7.58%), Space: 12.2 MB (91.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,13 +1,24 @@
 class Solution(object):
     def sortColors(self, nums):
         n=len(nums)
-        for i in range(0,n):
-            min_index=i
-            for j in range(i+1,n):
-                if nums[j]<nums[min_index]:
-                    min_index=j
-            #swapping
-            nums[i],nums[min_index]=nums[min_index],nums[i]
+        has_swapped=True
+        while has_swapped:
+            has_swapped=False
+            for i in range(n-1):
+                if nums[i]>nums[i+1]:
+                    nums[i],nums[i+1]=nums[i+1],nums[i]
+                    has_swapped=True
         return nums
+#Bubble Sort - Time Complexity - O(n^2) best case- O(N)
+#Space Complexity - O(1)
+        # n=len(nums)
+        # for i in range(0,n):
+        #     min_index=i
+        #     for j in range(i+1,n):
+        #         if nums[j]<nums[min_index]:
+        #             min_index=j
+        #     #swapping
+        #     nums[i],nums[min_index]=nums[min_index],nums[i]
+        # return nums
 #Time complexity - O(n^2)  Required optimization
 #Space complexity- o(1) inplace modification
